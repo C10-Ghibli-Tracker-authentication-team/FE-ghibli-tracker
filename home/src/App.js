81a178a5934d3e6f6c0b437e780aa1fca2dbece8
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-04-12 22:28:28 -0600

    src

---
+++ home/src/App.js
@@ -0,0 +1,21 @@
+import Menu from './components/Menu';
+import Films from './components/Films';
+
+
+//import './App.css';
+
+function App() {
+  return (
+
+    <>
+
+    <Menu />,
+    <Films />
+    
+    </>
+    
+
+  );
+}
+
+export default App;
