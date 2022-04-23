300c0ab703aaffb1c22e26b0cd680eec3e8006ff
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-04-22 18:57:58 -0600

    project update

---
+++ src/home/components/Films/CardsTablet.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Section, Image, Data, H2, H3, P, Rate, Toggle } from "../../Styles/CardsTabletStyles";
+import Stars from '../Stars';
+import ReactStars from "react-rating-stars-component";
+import FavIcon from "../ToggleHeart";
+
+
+const CardsTablet = ({image,title,year,description}) => {
+
+        return (
+            <>
+            <Section>,
+                <Image src={image} alt="film image"/>
+                <Data className="card_data">
+                    <H2 className="title">{title}</H2>
+                    <H3 className="year">{year}</H3>
+                    {<P className="description" id="description">{description}</P>}
+                    <Rate className="App">
+                        <   ReactStars {...Stars } />
+                        <Toggle className="FavIcon">
+                            <FavIcon />
+                        </Toggle>
+                    </Rate>
+                </Data>
+            </Section>
+            </>
+        )
+    
+}
+
+export default CardsTablet
