5b3aa751c3c9742b737451a61cc1a858212dae24
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-04-28 18:57:57 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import MovieApp from "./Film/index";
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="movie-details" element={<MovieApp />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
-
