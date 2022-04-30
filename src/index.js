669e2cc3ddea61d7229b2bcc34912061f9f40def
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-04-29 19:04:02 -0500

    call index.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-import App from './App';
+import './index.css';
+// import App from './App';
 import Account from './home/components/pages/Account';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     // <App />,
