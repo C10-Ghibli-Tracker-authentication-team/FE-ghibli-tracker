d57d08f4fd9167e09fac78b50d2af48c00758943
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-05-01 10:58:28 -0600

    update home components

---
+++ src/home/FilmsDesktop.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import CardsDesktop from './CardsDesktop';
+import '../index.css';
+
+const FilmsDesktop = () => {
+
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+        fetch('https://ghibliapi.herokuapp.com/films')
+        .then(r => r.json())
+        .then(r => setFilms(r));
+            
+
+    },  []);
+
+    return (
+        <div className="films">
+          {films.map((film) => {
+           return <CardsDesktop
+                    image={film.image}
+                    title={film.title}
+                    year={film.release_date}
+                    description={film.description}
+                    key={film.id}
+                    />;
+          })}
+        </div>
+      );
+    };
+
+export default FilmsDesktop;
