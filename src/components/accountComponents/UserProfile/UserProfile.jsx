2b92d993b9052f5b80ba9a2bafbb368e07ba3d80
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-05-04 06:48:22 -0500

    components structure

---
+++ src/components/accountComponents/UserProfile/UserProfile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import user_profile from './user_profile.jpg';
+import {
+  UserInfo,
+  Figure,
+  Image,
+  UserName,
+  UserLinks,
+  A,
+} from '../../Styles/UserProfileStyles';
+
+const UserProfile = ({ username }) => {
+  return (
+    <>
+    {/* USER INFORMATION */}
+      <UserInfo>
+        <Figure>
+          <Image src={user_profile} alt="user image profile" />
+        </Figure>
+        <UserName className="username">
+          {/* {username} */} Santiago Bastidas
+        </UserName>
+      </UserInfo>
+    {/* USER LINKS */}
+      <UserLinks>
+        <A>Scores</A>
+        <A>Passwords</A>
+      </UserLinks>
+    </>
+  );
+};
+
+export default UserProfile;
