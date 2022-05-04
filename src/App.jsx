e1025439455e72238f4903d961651c0f2b8b4fa4
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-05-04 18:53:59 -0500

    Move app and index components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './src/components/Navbar';
-import Home from './src/pages/Home';
-import Account from './src/pages/Account';
+import Home from './pages/Home';
+import Account from './pages/Account';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
