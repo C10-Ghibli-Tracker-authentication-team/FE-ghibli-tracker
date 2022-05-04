abc7dee6723a9fa093b4aa1075719a245acfa684
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-05-04 07:01:16 -0500

    pages structure and Account breakpoints

---
+++ src/pages/Account.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Breakpoint, BreakpointProvider } from 'react-socks';
+import { setDefaultBreakpoints } from 'react-socks';
+import UserProfile from '../components/accountComponents/UserProfile/UserProfile';
+import UserProfileTablet from '../components/accountComponents/UserProfile/UserProfileTablet';
+import UserProfileDesktop from '../components/accountComponents/UserProfile/UserProfileDesktop';
+
+setDefaultBreakpoints([
+  { xsmall: 0 }, // all mobile devices
+  { small: 576 }, // mobile devices (not sure which one's this big)
+  { medium: 768 }, // ipad, ipad pro, ipad mini, etc
+  { large: 992 }, // smaller laptops
+]);
+
+function Account() {
+  return (
+    <>
+      <BreakpointProvider>
+        <Breakpoint small down>
+          {/* <Navbar />, */}
+          <UserProfile />,
+        </Breakpoint>
+
+        <Breakpoint medium only>
+          {/* <NavbarTablet />, */}
+          <UserProfile />,
+        </Breakpoint>
+
+        <Breakpoint large up>
+          {/* <NavbarDesktop />, */}
+          <UserProfile />,
+        </Breakpoint>
+      </BreakpointProvider>
+    </>
+  );
+}
+
+export default Account;
