60c9348bea3ed715aeb249a40bc22746ed67b72a
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-05-04 07:33:36 -0500

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import user from './User-icon.png';
+import search from './search.png';
+import {
+  Container,
+  User,
+  UserIcon,
+  Search,
+  SearchIcon,
+} from '../../Styles/NavbarStyles';
+
+const Navbar = () => {
+  return (
+    <>
+      <Container className="container-menu">
+        <User href="/toAccount">
+          <UserIcon src={user} alt="account" className="user-icon" />
+        </User>
+
+        <Search href="/ToSearch">
+          <SearchIcon src={search} alt="search" className="search-icon" />
+        </Search>
+      </Container>
+    </>
+  );
+};
+
+export default Navbar;
