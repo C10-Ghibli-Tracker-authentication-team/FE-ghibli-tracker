c614e1b18198d3b61006d77e28b01b98ca823a52
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-05-04 19:03:40 -0500

    Refactor Films component calls

--- src/components/HomeComponents/Films/FilmsDesktop.jsx
+++ src/components/HomeComponents/Films/FilmsDesktop.jsx
@@ -1,32 +1,30 @@
-import React, { useState, useEffect } from "react";
-import CardsDesktop from './CardsDesktop';
-import './Films.css';
+import React, { useState, useEffect } from 'react';
+import CardsDesktop from '../Cards/CardsDesktop';
 
 const FilmsDesktop = () => {
+  const [films, setFilms] = useState([]);
 
-    const [films, setFilms] = useState([]);
+  useEffect(() => {
+    fetch('https://ghibliapi.herokuapp.com/films')
+      .then((r) => r.json())
+      .then((r) => setFilms(r));
+  }, []);
 
-    useEffect(() => {
-        fetch('https://ghibliapi.herokuapp.com/films')
-        .then(r => r.json())
-        .then(r => setFilms(r));
-            
-
-    },  []);
-
-    return (
-        <div className="films">
-          {films.map((film) => {
-           return <CardsDesktop
-                    image={film.image}
-                    title={film.title}
-                    year={film.release_date}
-                    description={film.description}
-                    key={film.id}
-                    />;
-          })}
-        </div>
-      );
-    };
+  return (
+    <div className="films">
+      {films.map((film) => {
+        return (
+          <CardsDesktop
+            image={film.image}
+            title={film.title}
+            year={film.release_date}
+            description={film.description}
+            key={film.id}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default FilmsDesktop;
