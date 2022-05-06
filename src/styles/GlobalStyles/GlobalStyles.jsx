d2f0a9436e0fccb858e8369a7d1296fb0ce22b4a
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-05-06 07:22:58 -0500

    Global styles with styled components

---
+++ src/styles/GlobalStyles/GlobalStyles.jsx
@@ -0,0 +1,30 @@
+export const Fonts = {
+  'Headline-1': 'bold 9.2rem "Montserrat"',
+  'Headline-2': 'bold 6.4rem "Montserrat"',
+  'Headline-3': 'bold 4.8rem "Montserrat"',
+  'Headline-4': 'bold 3.6rem "Montserrat"',
+  'Headline-5': 'normal 2.4rem "Montserrat"',
+  'Headline-6': 'normal 2rem "Montserrat"',
+  'Subtitle-1': 'normal 1.6rem "Montserrat"',
+  'Subtitle-2': 'normal 1.4rem "Montserrat"',
+  'Body-1': '1.6rem',
+  'Body-2': '1.4rem',
+  'BUTTON': '1.4rem',
+  'Caption': '1.2rem',
+  'OVERLINE': '1.6rem',
+}
+
+export const Colors = {
+  Primary: '#001219',
+  Secondary: '#005f73',
+  Tertiary: '#0a9396',
+  Light: '#94d2bd',
+  Orange: '#ca6702',
+  Red: '#ae2012',
+  White: '#fff',
+  LightGrey: '#d4d4d4',
+  SilverChalice: '#ababab',
+  DavysGrey: '#555555',
+  Jet: '#2a2a2a',
+  Black: '#000',
+}
