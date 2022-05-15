af943b311df2afa5df1a3ebef0d0510f6838763b
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-05-14 21:49:20 -0500

    calls user facebook api

--- src/components/AccountComponents/UserProfile/index.jsx
+++ src/components/AccountComponents/UserProfile/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import user_profile from '../assets/user_profile.jpg';
 import {
   UserInfo,
@@ -8,6 +8,15 @@ import {
 } from '../../../styles/UserProfile/UserProfileStyles';
 
 const UserProfile = () => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch('https://be-ghibli-tracker.herokuapp.com/auth/facebook')
+      .then((r) => console.log(r.json()))
+      .then((u) => setUser(u));
+  }, []);
+
+  console.log(user);
   return (
     <>
       <UserInfo>
