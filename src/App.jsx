c434104546115a64ea722ee6cee2f0a38d80c85b
santiagobr <santiagobastidasrodriguez@gmail.com>
2022-06-28 23:18:12 -0500

    🚧  SignUp: re-building the sign up component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import Account from './pages/Account';
 import Login from './components/LoginComponents/Login';
 import SingUp from './components/LoginComponents/SingUp';
-import Movie from './pages/Movie';
+// import Movie from './pages/Movie';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavbarArrow from './components/NavbarComponents/NavbarArrow';
 import NavbarHome from './components/NavbarComponents/NavbarHome';
@@ -13,9 +13,9 @@ import NavbarSearch from './components/NavbarComponents/NavbarSearch';
 function App() {
   const [token, setToken] = useState();
 
-  if(!token) {
-    return <Login setToken={()=>setToken()} />
-  }
+ /*  // if(!token) {
+  //   return <Login setToken={()=>setToken()} />
+  // } */
   return (
     <>
       <Router>
@@ -27,9 +27,9 @@ function App() {
         </Routes>
         <Routes> 
           <Route path="/" element={<Home />} />
-          <Route path="movie/:id" element={<Movie />}/>
+          {/* <Route path="movie/:id" element={<Movie />}/> */}
           <Route path="/account" element={<Account />} />
-          <Route path="/singup" element={<SingUp />} />
+          <Route path="/signup" element={<SingUp />} />
         </Routes>
       </Router>
     </>
--- src/components/LoginComponents/SingUp.jsx
+++ src/components/LoginComponents/SingUp.jsx
@@ -1,90 +1,88 @@
-import React, { useState } from "react";
-import user from "./user.png";
-import key from "./key.png";
-import twitter from "./twitter.png";
-import facebook from "./facebook.png";
-import { 
-    FormInner, 
-    Title,
-    Subtitle,
-    MailContainer, 
-    UserContainer, 
-    PasswordContainer, 
-    LoginBtn, 
-    ChangeView,
-    Twitter,
-    TwitterLogo,
-    Facebook,
-    FacebookLogo
-} from "../../styles/Login/LoginFormStyles";
+import React, { useState } from 'react';
+import user from './user.png';
+import key from './key.png';
+import twitter from './twitter.png';
+import facebook from './facebook.png';
+import {
+  FormInner,
+  Title,
+  Subtitle,
+  MailContainer,
+  UserContainer,
+  PasswordContainer,
+  LoginBtn,
+  ChangeView,
+  Twitter,
+  TwitterLogo,
+  Facebook,
+  FacebookLogo,
+} from '../../styles/Login/LoginFormStyles';
 
-const Register = () => {
-  const [registerMail, setRegisterMail] = useState("");
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  (async () => {
-    const rawResponse = await fetch('https://estudio-ghibli-2022.herokuapp.com/signup/', {
+const getData = async (email, user, password) => {
+  const rawResponse = await fetch(
+    'https://estudio-ghibli-2022.herokuapp.com/signup/',
+    {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        email: registerMail,
-        userName: registerUsername,
-        password: registerPassword})
-    });
-    const content = await rawResponse.json();
-  
-    console.log(content);
-  }
-)() 
+        email: email,
+        userName: user,
+        password: password,
+      }),
+    }
+  );
+  const content = await rawResponse.json();
 
-    return (
-        <FormInner>
-            <Title>Studio Ghibli Tracker</Title>
-            <Subtitle>Register</Subtitle>
-            <MailContainer className="MailContainer">
-                <label htmlFor="email">
-                    Email
-                </label>
-                <input 
-                    placeholder="example@mail.com"
-                    onChange={(e) => setRegisterMail(e.target.value)} 
-                    />
-                <img src={user} alt="user logo" />
-            </MailContainer>
-            <UserContainer className="UserContainer">
-                <label htmlFor="user">
-                    User
-                </label>
-                <input 
-                    placeholder="Your User"
-                    onChange={(e) => setRegisterUsername(e.target.value)} 
-                    />
-                <img src={user} alt="user logo" />
-            </UserContainer>
-            <PasswordContainer className="PasswordContainer">
-                <label htmlFor="password">
-                    Password
-                </label>
-                <input 
-                     placeholder="******"
-                     onChange={(e) => setRegisterPassword(e.target.value)}
-                    />
-                <img src={key} alt="key logo" />
-            </PasswordContainer>
+  console.log(content);
+};
 
-            <LoginBtn type="button" className="LoginBtn" onClick={Register}>Submit</LoginBtn>
+const Register = () => {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerUser, setRegisterUser] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
 
-            <ChangeView>Or login</ChangeView>
-            
 
-            <Twitter type="submit" value="Connect with Twitter" className="twitter" />
-            <TwitterLogo src={twitter} alt="Twitter logo" className="TwitterLogo" />
-            <Facebook type="submit" value="Connect with Facebook" className="Facebook" />
-            <FacebookLogo src={facebook} alt="Facebook logo" className="FacebookLogo" />
-        </FormInner>
-    )
-  };
+  return (
+    <FormInner onSubmit={() => getData((registerEmail, registerUser, registerPassword))} >
+      <Title>Studio Ghibli Tracker</Title>
+      <Subtitle>Register</Subtitle>
+      <MailContainer className='MailContainer'>
+        <label htmlFor='email'>Email</label>
+        <input placeholder='example@mail.com' onChange={(e) => setRegisterEmail(e.target.value)}/>
+        <img src={user} alt='user logo' />
+      </MailContainer>
+      <UserContainer className='UserContainer'>
+        <label htmlFor='user'>User</label>
+        <input placeholder='Your User' onChange={(e) => setRegisterUser(e.target.value)}/>
+        <img src={user} alt='user logo' />
+      </UserContainer>
+      <PasswordContainer className='PasswordContainer'>
+        <label htmlFor='password'>Password</label>
+        <input placeholder='******' onChange={(e) => setRegisterPassword(e.target.value)}/>
+        <img src={key} alt='key logo' />
+      </PasswordContainer>
+      <LoginBtn type='submit' className='LoginBtn'>
+        Enviar
+      </LoginBtn>
+
+      <ChangeView>Or login</ChangeView>
+
+      <Twitter type='submit' value='Connect with Twitter' className='twitter' />
+      <TwitterLogo src={twitter} alt='Twitter logo' className='TwitterLogo' />
+      <Facebook
+        type='submit'
+        value='Connect with Facebook'
+        className='Facebook'
+      />
+      <FacebookLogo
+        src={facebook}
+        alt='Facebook logo'
+        className='FacebookLogo'
+      />
+    </FormInner>
+  );
+};
 export default Register;
