1318411b9b8a7164867d8c94b346c225c4c63e9d
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-07-17 18:33:53 -0600

    Solving conflicts with FavIcon in the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import Home from './pages/Home';
 import Account from './pages/Account';
+import Signin from './pages/Signin';
+import Register from './pages/Register';
 import Login from './components/LoginComponents/Login';
-import SingUp from './components/LoginComponents/SingUp';
+import SignUp from './components/LoginComponents/SignUp';
 // import Movie from './pages/Movie';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavbarArrow from './components/NavbarComponents/NavbarArrow';
 import NavbarHome from './components/NavbarComponents/NavbarHome';
 import NavbarFilter from './components/NavbarComponents/NavbarFilter';
 import NavbarSearch from './components/NavbarComponents/NavbarSearch';
+import useToken from './components/LoginComponents/useToken';
 
 function App() {
-  const [token, setToken] = useState();
-
- /*  // if(!token) {
-  //   return <Login setToken={()=>setToken()} />
-  // } */
+    
   return (
     <>
       <Router>
@@ -28,9 +27,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="movie/:id" element={<Movie />}/> */}
+          <Route path="/login" element={<Login />} />
           <Route path="/account" element={<Account />} />
-          <Route path="/signup" element={<SingUp />} />
+          <Route path="/register" element={<SignUp />} />
         </Routes>
       </Router>
     </>
