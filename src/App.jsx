084250a0a70baf0087051cd4728778bb03fac2ac
Santiago Bastidas <santiagobastidasrodriguez@gmail.com>
2022-07-22 21:39:09 -0500

    ✏️ Fix de typos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import React, { useState } from "react";
 import Home from './pages/Home';
 import Account from './pages/Account';
-import Signin from './pages/Signin';
-import Register from './pages/Register';
 import Login from './components/LoginComponents/Login';
-import SingUp from './components/LoginComponents/SingUp';
-import Movie from './pages/Movie';
+import SignUp from './components/LoginComponents/SignUp';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavbarArrow from './components/NavbarComponents/NavbarArrow';
 import NavbarHome from './components/NavbarComponents/NavbarHome';
 import NavbarFilter from './components/NavbarComponents/NavbarFilter';
 import NavbarSearch from './components/NavbarComponents/NavbarSearch';
-import useToken from './components/LoginComponents/useToken';
+// ! import useToken from './components/LoginComponents/useToken';
 
 function App() {
-    
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/account' element={<NavbarArrow/>}/>
-          <Route path='/movie/:id' element={<NavbarArrow/>}/>
-          <Route path='/' element={<NavbarHome/>}/>
-          <Route path='/filter' element={<NavbarFilter/>}/>
-          <Route path='/search' element={<NavbarSearch/>}/>
+          <Route path='/account' element={<NavbarArrow />} />
+          <Route path='/movie/:id' element={<NavbarArrow />} />
+          <Route path='/' element={<NavbarHome />} />
+          <Route path='/filter' element={<NavbarFilter />} />
+          <Route path='/search' element={<NavbarSearch />} />
         </Routes>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/register" element={<SignUp />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/account' element={<Account />} />
+          <Route path='/register' element={<SignUp />} />
         </Routes>
       </Router>
     </>
--- src/components/LoginComponents/Login.jsx
+++ src/components/LoginComponents/Login.jsx
@@ -1,94 +1,100 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import userLogo from "./user.png";
-import key from "./key.png";
-import twitter from "./twitter.png";
-import facebook from "./facebook.png";
-import { 
-    FormInner, 
-    Title,
-    Subtitle, 
-    Container, 
-    LoginBtn, 
-    Twitter,
-    TwitterLogo,
-    Facebook,
-    FacebookLogo,
-    Redirect,
-    RedirectLabel,
-    RedirectLink
-} from "../../styles/Login/LoginFormStyles";
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import userLogo from './user.png';
+import key from './key.png';
+import twitter from './twitter.png';
+import facebook from './facebook.png';
+import {
+  FormInner,
+  Title,
+  Subtitle,
+  Container,
+  LoginBtn,
+  Twitter,
+  TwitterLogo,
+  Facebook,
+  FacebookLogo,
+  Redirect,
+  RedirectLabel,
+  RedirectLink,
+} from '../../styles/Login/LoginFormStyles';
 
 const Login = () => {
-      const [username, setUsername] = useState('')
-      const [password, setPassword] = useState('')
-      let navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  let navigate = useNavigate();
 
-      const onSubmitHandler = (async () => {
-         const rawResponse = await fetch('https://estudio-ghibli-2022.herokuapp.com/login', {
-           method: 'POST',
-           headers: {
-             'Accept': 'application/json',
-             'Content-Type': 'application/json',
-             'Authorization': 'Bearer ' + username.token
-           },
-           body: JSON.stringify({
-             email: username,
-             password: password})
-         });
-         const content = await rawResponse.json();
+  const onSubmitHandler = async () => {
+    const rawResponse = await fetch(
+      'https://estudio-ghibli-2022.herokuapp.com/login',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + username.token,
+        },
+        body: JSON.stringify({
+          email: username,
+          password: password,
+        }),
+      }
+    );
+    const content = await rawResponse.json();
 
-         console.log(content);
-       })
-
-    return (
-      <FormInner>
-        <Title>Studio Ghibli Tracker</Title>
-        <Subtitle>Login</Subtitle>
-            <Container className="MailContainer">
-                <label htmlFor="email">
-                    Email
-                </label>
-                <input 
-                    placeholder="example@mail.com"
-                    onChange={(e) => setUsername(e.target.value)} 
-                    />
-                <img src={userLogo} alt="user logo" />
-            </Container>
-            <Container className="PasswordContainer">
-                <label htmlFor="password">
-                    Password
-                </label>
-                <input 
-                     placeholder="******"
-                     onChange={(e) => setPassword(e.target.value)}
-                    />
-                <img src={key} alt="key logo" />
-            </Container>
-
-            <LoginBtn
-              type="button" 
-              className="LoginBtn" 
-              onClick={(e) => {
-                  e.preventDefault();
-                  onSubmitHandler();
-                  navigate("/");
-                                  }}
-            >
-              Login</LoginBtn>
-
-            <Redirect>
-                    <RedirectLabel>Don't have an account?</RedirectLabel>
-                    <RedirectLink to="/register">Register</RedirectLink>
-                </Redirect>
-
-
-            <Twitter type="submit" value="Connect with Twitter" className="twitter" />
-            <TwitterLogo src={twitter} alt="Twitter logo" className="TwitterLogo" />
-            <Facebook type="submit" value="Connect with Facebook" className="Facebook" />
-            <FacebookLogo src={facebook} alt="Facebook logo" className="FacebookLogo" />
-        </FormInner>
-    )
+    console.log(content);
   };
+
+  return (
+    <FormInner>
+      <Title>Studio Ghibli Tracker</Title>
+      <Subtitle>Login</Subtitle>
+      <Container className='MailContainer'>
+        <label htmlFor='email'>Email</label>
+        <input
+          placeholder='example@mail.com'
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <img src={userLogo} alt='user logo' />
+      </Container>
+      <Container className='PasswordContainer'>
+        <label htmlFor='password'>Password</label>
+        <input
+          placeholder='******'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <img src={key} alt='key logo' />
+      </Container>
+
+      <LoginBtn
+        type='button'
+        className='LoginBtn'
+        onClick={(e) => {
+          e.preventDefault();
+          onSubmitHandler();
+          navigate('/');
+        }}>
+        Login
+      </LoginBtn>
+
+      <Redirect>
+        <RedirectLabel>Don't have an account?</RedirectLabel>
+        <RedirectLink to='/register'>Register</RedirectLink>
+      </Redirect>
+
+      <Twitter type='submit' value='Connect with Twitter' className='twitter' />
+      <TwitterLogo src={twitter} alt='Twitter logo' className='TwitterLogo' />
+      <Facebook
+        type='submit'
+        value='Connect with Facebook'
+        className='Facebook'
+      />
+      <FacebookLogo
+        src={facebook}
+        alt='Facebook logo'
+        className='FacebookLogo'
+      />
+    </FormInner>
+  );
+};
 export default Login;
