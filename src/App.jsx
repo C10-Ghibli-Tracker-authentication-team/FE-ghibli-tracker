5b1070a744e718fa4debf6ae4b7c4f6b25cfcdf0
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-08-22 11:23:20 -0600

    🚧 Creating a landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,31 @@
+import React from 'react';
 import Home from "./pages/Home";
 import Account from "./pages/Account";
-import Login from "./components/LoginComponents/Login";
-import Movie from "./pages/Movie";
+import Register from "./components/LoginComponents/SignUp";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavbarArrow from "./components/NavbarComponents/NavbarArrow";
 import NavbarHome from "./components/NavbarComponents/NavbarHome";
 import NavbarFilter from "./components/NavbarComponents/NavbarFilter";
 import NavbarSearch from "./components/NavbarComponents/NavbarSearch";
-import PrivateRoutes from "./utils/PrivateRoutes";
-import { AuthProvider } from "./context/AuthContext";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <>
       <Router>
-        <AuthProvider>
           <Routes>
             <Route path="/account" element={<NavbarArrow />} />
-            <Route path="/" element={<NavbarHome />} />
+            <Route path="/home" element={<NavbarHome />} />
             <Route path="/filter" element={<NavbarFilter />} />
             <Route path="/search" element={<NavbarSearch />} />
           </Routes>
 
           <Routes>
-            <Route element={<PrivateRoutes />}>
-              <Route path="/" element={<Home />} />
-            </Route>
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Landing />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/account" element={<Account />} />
             <Route path="/register" element={<Register />} />
           </Routes>
-        </AuthProvider>
 
         <Routes>
           <Route path="/account" element={<NavbarArrow />} />
