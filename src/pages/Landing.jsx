eebc82e67bb044cb4f1050a079577cfec53981e6
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-08-24 20:01:44 -0600

    🚧  Components for a container, cover image, description and buttons of the landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,21 +3,29 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from '../components/LoginComponents/LoginButton';
 import LogoutButton from '../components/LoginComponents/LogOut';
 import { Profile } from "../components/LoginComponents/Profile";
+import { Container, Image, Welcome, Join } from "../styles/Landing/LandingStyles";
+import StudioGhibli from "./StudioGhibli.png";
+
 
 function Landing() {
     const { isAuthenticated } = useAuth0();
 
     return (
-      <div>
+      <Container>
           {isAuthenticated ? (
             <>
               <Profile />
               <LogoutButton />
             </>
           ) : (
+            <>
+            <Image src={StudioGhibli} alt="Studio Ghibli" />
+            <Welcome>Welcome to Studio Ghibli Tracker, your new favorite app</Welcome> 
+            <Join>Join to our community and start this adventure interacting with all Studio Ghibli movies</Join>
             <LoginButton />
+            </>
           )}
-      </div>
+      </Container>
     );
 }
 
