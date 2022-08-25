f391f27a2985bd3f1ee8f0b249c5971abafb98b6
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-08-24 20:45:17 -0600

    🚧 Components for login and logout buttons

--- src/components/LoginComponents/LoginButton.js
+++ src/components/LoginComponents/LoginButton.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { LoginBtn } from "../../styles/Landing/LandingStyles";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <LoginBtn onClick={() => loginWithRedirect()}>Log In</LoginBtn>;
 };
 
 export default LoginButton;
