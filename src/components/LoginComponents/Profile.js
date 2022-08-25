cbe2bbf12a4cc5ee39546adb8c48e4715bd3bca6
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-08-24 20:09:10 -0600

    🚧 Adding profile components image, name and mail

--- src/components/LoginComponents/Profile.js
+++ src/components/LoginComponents/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Container, Image, Name, Email } from "../../styles/Landing/ProfileStyles";
 
 
 
@@ -38,17 +39,11 @@ export const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        <h3>User Metadata</h3>
-        {userMetadata ? (
-          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
-        ) : (
-          "No user metadata defined"
-        )}
-      </div>
+      <Container>
+        <Image src={user.picture} alt={user.name} />
+        <Name>{user.name}</Name>
+        <Email>{user.email}</Email>
+      </Container>
     )
   );
 
