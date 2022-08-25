5c65b036ceb460d5a54bcd3ee8b8e51b172bff0c
gabrielnavarros <gnavarro.soberanes@gmail.com>
2022-08-24 20:39:53 -0600

    💄 Styled components for profile info image, name, email and logout button

---
+++ src/styles/Landing/ProfileStyles.jsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  @media screen and (min-width: 478px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const Image = styled.img`
+    box-sizing: border-box;
+    margin-left: -.25rem;
+    border-radius: 2rem;
+    margin-top: 5rem;
+    width: 100%;
+    object-fit: cover;
+`;
+
+export const Name = styled.h2`
+  position: absolute;
+  width: 39.6rem;
+  height: 11.6rem;
+  font: var(--Headline-6);
+  text-align: center;
+  color: var(--Light);
+  margin-top: 20rem;
+`;
+
+export const Email = styled.p`
+  position: absolute;
+  width: 39.6rem;
+  height: 11.6rem;
+  font: var(--Headline-6);
+  text-align: center;
+  color: var(--Light);
+  margin-top: 30rem;
+`;
+
+export const LogOutBtn = styled.button`
+  width: 17.1rem;
+  height: 4.8rem;
+  margin-top: 35rem;
+  border: none;
+  border-radius: 1.5rem;
+  background-color: var(--Orange);
+  color: var(--White);
+  font: var(--Subtitle-1);
+  letter-spacing: 0.125em;
+  cursor: pointer;
+`;
+
